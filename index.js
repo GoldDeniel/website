f82f3f16a51bfe55de276dca21885eeb978426a0
GoldDeniel <aranyadhun@gmail.com>
2023-02-23 01:04:48 +0100

    Initial commit

---
+++ index.js
@@ -0,0 +1,10 @@
+function gotoProject(projectID) {
+    
+    // Get the project name from the database
+    var projectName = projectID;
+    
+    // Redirect to the project page
+    // window.location.href = "project.php?project=" + projectName;
+    
+}
+ 
